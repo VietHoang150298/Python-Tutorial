a257b7029940b90c97094a7c1a73bfd18a9caa69
HOANG <perfectdude1502@gmail.com>
2023-03-30 23:55:45 +0700

    first commit

---
+++ hieu.py
@@ -0,0 +1,11 @@
+you = ""
+if you == "":
+    robot_brain = "I can't hear you, try again"
+elif you == "today":
+    robot_brain = "Saturday"
+elif you == "hello":
+    robot_brain = "Hello Daniel"  
+else:
+    robot_brain = "I'm fine thank you and you?"  
+    
+print(robot_brain)
